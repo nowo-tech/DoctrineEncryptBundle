b9d893ccb2824ecbf20b9375c26d28725a6be9cf
Héctor Franco <hectorfranco@nowo.tech>
2026-02-25 13:48:20 +0100

    Refactor DoctrineEncryptExtension to improve dependency management
    - Replaced the deprecated Symfony\Component\HttpKernel\DependencyInjection\Extension with Symfony\Component\DependencyInjection\Extension in DoctrineEncryptExtension.php for better compatibility and adherence to current Symfony standards.

--- src/DependencyInjection/DoctrineEncryptExtension.php
+++ src/DependencyInjection/DoctrineEncryptExtension.php
@@ -9,8 +9,8 @@ use Nowo\DoctrineEncryptBundle\Encryptors\HaliteEncryptor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 use function array_key_exists;
 use function count;
