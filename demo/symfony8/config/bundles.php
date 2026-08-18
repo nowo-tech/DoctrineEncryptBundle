1d780ee0182712b4e70f3fadaff9bb0b89500f90
Héctor Franco <hectorfranco@nowo.tech>
2026-08-18 11:22:22 +0200

    Bump demo HotReloadBundle to ^1.4 and enable FrankenPHP hot reload.

--- demo/symfony8/config/bundles.php
+++ demo/symfony8/config/bundles.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
 use Nowo\DoctrineEncryptBundle\NowoDoctrineEncryptBundle;
+use Nowo\HotReloadBundle\NowoHotReloadBundle;
 use Nowo\TwigInspectorBundle\NowoTwigInspectorBundle;
 use Symfony\Bundle\DebugBundle\DebugBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
+use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
 
 return [
     FrameworkBundle::class           => ['all' => true],
@@ -18,5 +20,7 @@ return [
     DoctrineFixturesBundle::class    => ['dev' => true, 'test' => true],
     DebugBundle::class               => ['dev' => true],
     WebProfilerBundle::class         => ['dev' => true, 'test' => true],
+    NowoHotReloadBundle::class   => ['dev' => true, 'test' => true],
     NowoTwigInspectorBundle::class   => ['dev' => true, 'test' => true],
+    TwigExtraBundle::class => ['all' => true],
 ];
